7b686a649974ba7054423105c6a3358dfa8e61a8
tukasz <tukasz@users.noreply.github.com>
2020-09-15 12:22:26 +0200

    Updated action logic

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
 import { getInput, info, setFailed } from '@actions/core';
 import { getOctokit } from '@actions/github';
+import { OctokitResponse, ReposMergeResponseData } from '@octokit/types';
 
 const GITHUB_TOKEN = getInput('GITHUB_TOKEN');
 const GITHUB_LOGIN = getInput('GITHUB_LOGIN');
 const owner = getInput('owner');
 const repo = getInput('repo');
-const sourceBranch = getInput('source-branch');
-const targetBranch = getInput('target-branch');
+const head = getInput('source-branch');
+const base = getInput('target-branch');
 const octokit = getOctokit(GITHUB_TOKEN);
 
 const main = async (): Promise<void> => {
   info(
-    `Running direct GitHub merge of ${owner}/${repo} ${sourceBranch} -> ${targetBranch} as: ${GITHUB_LOGIN}`
+    `Running direct GitHub merge of ${owner}/${repo} ${head} -> ${base} as: ${GITHUB_LOGIN}`
   );
 
-  const branches = await octokit.repos.listBranches({
+  const res: OctokitResponse<ReposMergeResponseData> = await octokit.repos.merge({
     owner,
     repo,
-  });
+    base,
+    head,
+    commit_message: `Automatic merge of  ${head} -> ${base}`
+  }) as OctokitResponse<ReposMergeResponseData>
 
-  info(`Branches: ${JSON.stringify(branches, null, 2)}`);
+  info(`Merged ${head} -> ${base} (${res.data.sha})`);
 };
 
 main().catch((error: Error): void => {
